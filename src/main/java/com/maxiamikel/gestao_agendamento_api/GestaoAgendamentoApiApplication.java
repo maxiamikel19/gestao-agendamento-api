6f6d794b2e0b56d9ad8193de64970fc862317546
maxiamikel19 <maxloversist19@gmail.com>
2025-03-21 14:23:23 -0300

    Inicial commit

---
+++ src/main/java/com/maxiamikel/gestao_agendamento_api/GestaoAgendamentoApiApplication.java
@@ -0,0 +1,13 @@
+package com.maxiamikel.gestao_agendamento_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoAgendamentoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoAgendamentoApiApplication.class, args);
+	}
+
+}
